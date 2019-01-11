aaf38cc5d33c5ee39a409e0f0943047c7c6441cc
Kevin Wang <kevinwang300@berkeley.edu>
2019-01-10 16:21:10 -0800

    refactor react app to root

---
+++ build/precache-manifest.c588494b88ab2175c9d97f9941dcc8eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fea8a13b74e18761f62d",
+    "url": "/static/js/main.fea8a13b.chunk.js"
+  },
+  {
+    "revision": "d576848222b8f36aeee8",
+    "url": "/static/js/1.d5768482.chunk.js"
+  },
+  {
+    "revision": "fea8a13b74e18761f62d",
+    "url": "/static/css/main.e0a90909.chunk.css"
+  },
+  {
+    "revision": "1d869df1f6ee877ed0779486b69c4caa",
+    "url": "/index.html"
+  }
+];
